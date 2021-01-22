a5cf3cbe08f5077706ecfcaa69bbf80d58da18d4
yanagidahana <hana878787zzz@gmail.com>
2021-01-22 20:37:18 +0900

    デフォルト変更タイトル

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -5,7 +5,7 @@
 		<div class="col-sm-offset-2 col-sm-8">
 			<div class="panel panel-default">
 				<div class="panel-heading">
-					New Book
+					書籍を追加
 				</div>
 
 				<div class="panel-body">
@@ -18,7 +18,7 @@
 
 						<!-- Book Name -->
 						<div class="form-group">
-							<label for="task-name" class="col-sm-3 control-label">Book</label>
+							<label for="task-name" class="col-sm-3 control-label">書籍タイトル</label>
 
 							<div class="col-sm-6">
 								<input type="text" name="name" id="book-name" class="form-control" value="{{ old('book') }}">
